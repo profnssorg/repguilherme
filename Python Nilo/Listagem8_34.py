0ff61105ca32f5d407cd55ef6dafc01ef3a1035d
GuilhermeMendes99 <33981162+GuilhermeMendes99@users.noreply.github.com>
2017-11-26 16:20:20 -0200

    Update Listagem8_34.py

--- Python Nilo/Listagem8_34.py
+++ Python Nilo/Listagem8_34.py
@@ -1,8 +1,20 @@
-#------------------------------------------------------
-# Função de validação (a ser importada em outro módulo)
-#------------------------------------------------------
+## Nome do Programa: Validação de números inteiros
+## Autor: Guilherme Mendes
+## Descrição: Valida a resposta a um input como sendo um número inteiro dentro da faixa especificada
+## Versão: 1.0
+## Data: 26/11/2017
+
+
+## Definição de funções
 
 def valida_inteiro(mensagem, mínimo, máximo):
+    '''
+    Valida a resposta a um input como sendo um número inteiro dentro da faixa especificada
+        mensagem : input a ser exibido
+        mínimo : valor mínimo aceitável
+        máximo : valor máximo aceitável
+    '''
+    
     while True:
         try:
             v = int(input(mensagem))
